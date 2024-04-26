c1c3a0f1ba6649aa7bf875ddfca0f2e323623e55
pol.saumell <pol.saumell@estudiantat.upc.edu>
2024-04-26 09:33:28 +0200

    compila xo funciona?

--- Labs/lab2/LAB2/proyecto_1/cache_con_interface_bus/cache_con_interface_proc/cache/controlador/CODIGO/controlador.vhd
+++ Labs/lab2/LAB2/proyecto_1/cache_con_interface_bus/cache_con_interface_proc/cache/controlador/CODIGO/controlador.vhd
@@ -39,7 +39,7 @@ derechos_acceso <= '1' when (s_estado.AF and s_estado.EST) = '1' else '0';
 	process (reloj, pcero)
 	begin
 		if pcero = '1' then
-			estado <= DES0;
+			prxestado <= DES0;
 		elsif (rising_edge(reloj)) then
 			case estado is
 				when DES0		=>
@@ -118,58 +118,82 @@ derechos_acceso <= '1' when (s_estado.AF and s_estado.EST) = '1' else '0';
 
 			end case;
 		end if;
-		estado <= prxestado;
 	end process;
 	
+	estado <= prxestado;
 
 	-- Toca acabar // Obviament està xungo però només cal llegir procedimientos_control_sador_pkg.vhd
-	process (estado)
-		variable s_control_s: tp_contro_cam_cntl;
-		variable estat: std_logic;
-		variable resp_s: tp_contro_s;
-		variable pet_m_s: tp_cntl_memoria_s;
+	logi_sal: process(estado, pet, derechos_acceso, resp_m)
+	variable v_s_control: tp_contro_cam_cntl;
+	variable v_pet_m: tp_cntl_memoria_s;
+	variable v_resp: tp_contro_s;
 	begin
-			case estado is
-				when DES0		=>
-					--nada
-				when INI 		=>
-					--nada
-				when ESCINI		=>
-					actualizar_dato(s_control_s);
-					actualizar_etiqueta(s_control_s);
-					actualizar_estado(s_control_s, estat);
-				when DES		=>
-					interfaces_DES(resp_s);
-				when CMPETIQ	=>
-					lectura_etiq_estado(s_control_s);
-				when LEC 		=>
-					lectura_datos(s_control_s);
-				when HECHOL		=>
-					interfaces_HECHOL(resp_s);
-				when PML  		=>
-					peticion_memoria_lectura(pet_m_s);
-				when ESPL		=>
-					-- espera
-				when ESB		=>
-					actualizar_dato(s_control_s);
-					actualizar_etiqueta(s_control_s);
-					actualizar_estado(s_control_s, estat);
-				when PMEA		=>
-					peticion_memoria_escritura(pet_m_s);
-
-				when ESPEA		=>
-					--espera
-				when ESCP		=>
-					actualizar_dato(s_control_s);
-				when PMEF		=>
-					peticion_memoria_escritura(pet_m_s);
-				when ESPEF		=>
-					-- espera
-				when HECHOE		=>
-					--nada
-			end case;
-			resp <= resp_s;
-			pet_m <= pet_m_s;
+		 por_defecto(v_s_control, v_pet_m, v_resp);
+		 case estado is
+			  when DES0 =>
+					if not hay_peticion_procesador(pet) then
+						 interfaces_DES(v_resp);
+					end if;
+			  when INI =>
+					interfaces_en_CURSO(v_resp);
+					actualizar_etiqueta(v_s_control);
+					actualizar_estado(v_s_control, '1');
+					actualizar_dato(v_s_control);
+			  when ESCINI =>
+					interfaces_en_CURSO(v_resp);
+			  when DES =>
+					interfaces_DES(v_resp);
+					if hay_peticion_procesador(pet) then
+						 lectura_etiq_estado(v_s_control);
+					end if;
+			  when CMPETIQ =>
+					interfaces_en_CURSO(v_resp);
+					if es_acierto_lectura(pet, derechos_acceso) then
+						 lectura_datos(v_s_control);
+					elsif es_fallo_lectura(pet, derechos_acceso) then
+						 peticion_memoria_lectura(v_pet_m);
+					elsif es_acierto_escritura(pet, derechos_acceso) then
+						 peticion_memoria_escritura(v_pet_m);
+					elsif es_fallo_escritura(pet, derechos_acceso) then
+						 peticion_memoria_escritura(v_pet_m);
+					end if;
+			  when LEC =>
+					interfaces_en_CURSO(v_resp);
+			  when HECHOL =>
+					interfaces_HECHOL(v_resp);
+			  when PML =>
+					interfaces_en_CURSO(v_resp);
+			  when ESPL =>
+					interfaces_en_CURSO(v_resp);
+					if hay_respuesta_memoria(resp_m) then
+						 actu_datos_desde_bus(v_s_control);
+						 actualizar_etiqueta(v_s_control);
+						 actualizar_estado(v_s_control, '1');
+						 actualizar_dato(v_s_control);
+					end if;
+			  when ESB =>
+					interfaces_en_CURSO(v_resp);
+					lectura_datos(v_s_control);
+			  when PMEA =>
+					interfaces_en_CURSO(v_resp);
+					peticion_memoria_escritura(v_pet_m);
+			  when ESPEA =>
+					interfaces_en_CURSO(v_resp);
+					if hay_respuesta_memoria(resp_m) then
+						 actualizar_dato(v_s_control);
+					end if;
+			  when ESCP =>
+					interfaces_en_CURSO(v_resp);
+			  when PMEF =>
+					interfaces_en_CURSO(v_resp);
+			  when ESPEF =>
+					interfaces_en_CURSO(v_resp);
+			  when HECHOE =>
+					interfaces_HECHOE(v_resp);
+		 end case;
+		 s_control <= v_s_control after retardo_logica_salida;
+		 pet_m <= v_pet_m after retardo_logica_salida;
+		 resp <= v_resp after retardo_logica_salida;
 	end process;
-	
+
 end;
